6cf17f197c0f36f80a936df8dd24c23554ce1bed
Alexander Weber <web.alexander@web.de>
2018-07-16 16:18:48 +0200

    multiskin_model: Check skin format only if not provided by metadata file

--- multiskin_model/init.lua
+++ multiskin_model/init.lua
@@ -84,7 +84,9 @@ function multiskin_model.get_skin_format(file)
 end
 
 player_api.read_textures_and_meta(function(filename, skin)
-	local file = io.open(filename, "r")
-	skin.format = multiskin_model.get_skin_format(file)
-	file:close()
+	if not skin.format then
+		local file = io.open(filename, "r")
+		skin.format = multiskin_model.get_skin_format(file)
+		file:close()
+	end
 end)
